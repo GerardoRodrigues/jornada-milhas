f92830e9662433f7803832c84a673fcfe382bd4d
Gerardo <gerardofilho56@gmail.com>
2024-09-16 10:00:28 -0300

    Modal de passagens

---
+++ src/app/shared/botao-controle/botao-controle.component.ts
@@ -0,0 +1,12 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-controle',
+  templateUrl: './botao-controle.component.html',
+  styleUrl: './botao-controle.component.css'
+})
+export class BotaoControleComponent {
+  @Input() operacao: 'incremento' | 'decremento' = 'incremento';
+  @Input() src: string = '';
+  @Input() alt: string = '';
+}
