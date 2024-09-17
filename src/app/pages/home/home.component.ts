8d8a0f5057a54efcf8a09a3f4199ac78699d213b
Gerardo Rodrigues <gerardofilho56@gmail.com>
2024-09-17 11:14:25 -0300

    Lista de promoçoes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from '../../core/services/promocao.service';
+import { Promocao } from '../../core/types/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
+  promocoes!: Promocao[]
 
+  constructor(private servicePromo: PromocaoService){}
+  
+  ngOnInit(): void {
+    this.servicePromo.listar().subscribe(promos => {
+      this.promocoes = promos;
+    })
+  }
 }
