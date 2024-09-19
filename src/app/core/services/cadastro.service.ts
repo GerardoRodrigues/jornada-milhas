70b72329420b3e9a64d94cfc20daf2c61b86ef38
Gerardo Rodrigues <gerardofilho56@gmail.com>
2024-09-19 19:01:30 -0300

    Metodos de buscar e editar no cadastro service

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { PessoaUsuaria } from '../types/types';
@@ -15,5 +15,19 @@ export class CadastroService {
 
   cadastrar(pessoa: PessoaUsuaria): Observable<PessoaUsuaria>{
     return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro`, pessoa)
+  }
+
+  buscar(token: string): Observable<PessoaUsuaria>{
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.http.get<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, {headers})
+  }
+
+  editar(user: PessoaUsuaria, token: string): Observable<PessoaUsuaria>{
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.http.patch<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, user, {headers})
   }
 }
